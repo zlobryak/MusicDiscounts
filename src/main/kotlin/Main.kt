dd012f640c0eb51d0af0783347fbe1203e4fb08e
Zlobryak <clychnik@gmail.com>
2025-09-16 15:39:07 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+package ru.netology
+
+fun main() {
+    val purchasePrice = 15_001.0
+    val regularCustomer = true
+
+    val standartDiscount =
+        if (purchasePrice > 0 && purchasePrice <= 1000) purchasePrice
+        else if (purchasePrice > 1000 && purchasePrice <= 10_000) purchasePrice - 100
+        else if (purchasePrice > 10_000) purchasePrice * 0.95
+        else 0.0
+    println(
+        String.format(
+            "%.2f",
+            if (regularCustomer) standartDiscount * 0.99
+            else standartDiscount
+        )
+    )
+}
